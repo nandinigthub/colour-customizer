093d1dfa5096b18dc9daedab1d5edc6dd7aa8820
Nandini <nandinigattani9082@gmail.com>
2022-03-13 12:02:50 +0530

    .

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+document.querySelector('.switcher-btn').onlick =() =>{
+    document.querySelector('.color-switcher').classList.toggle('active');
+};
+let themeButtons = document.querySelectorAll('.theme-buttons');
+themeButtons.forEach(color =>{
+    color.addEventListener('click',() =>{
+        let dataColor = color.getAttribute('data-color');
+        document.querySelector(':root').style.setProperty("--primary-color")
+    })
+})
